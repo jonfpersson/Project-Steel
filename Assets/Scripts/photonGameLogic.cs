1c43199b881c6e285195ebba240b09b39be0628a
Jonathan <jonfpersson@gmail.com>
2019-07-07 03:05:09 +0200

    Added Photon API

---
+++ Assets/Scripts/photonGameLogic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class photonGameLogic : Photon.MonoBehaviour
+{
+
+    [SerializeField] private Text txt;
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private GameObject lobbyCamera;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("version 1");
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        Debug.Log("joined lobby");
+        // RoomOptions roomOptions = new RoomOptions();
+
+        PhotonNetwork.JoinOrCreateRoom("New", null, null);
+    }
+
+    public virtual void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate(player.name, spawnPoint.position, spawnPoint.rotation, 0);
+        lobbyCamera.SetActive(false);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+      //TEST
+      txt.text = PhotonNetwork.connectionStateDetailed.ToString();
+        
+    }
+
+}
