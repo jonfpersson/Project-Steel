262e119d1eb1b2e03c89f5efc3dc286d4c8b784d
Jonathan <jonfpersson@gmail.com>
2019-07-08 02:41:56 +0200

    Added multiplayer basics and terrain

---
+++ Assets/Scripts/playerNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerNetwork : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private MonoBehaviour[] playerControlScripts;
+
+    private PhotonView pw;
+
+    //private delegate void UpdateUI(string playerName);  
+    //private event UpdateUI updateUI;
+
+    string inputName = "Nisse";
+    // Start is called before the first frame update
+    void Start()
+    {
+        pw = GetComponent<PhotonView>();
+       // updateUI += FindObjectOfType<playerUI>().UpdateUI;
+
+        ini();
+    }
+
+    public void ini()
+    {
+        if (pw.isMine)
+        {
+
+        }
+        else
+        {
+            playerCamera.SetActive(false);
+            foreach(MonoBehaviour m in playerControlScripts)
+            {
+                m.enabled = false;
+            }
+        }
+
+        //updateUI(inputName);
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting){
+      //      stream.SendNext(name);
+        }
+        else if (stream.isReading)
+        {
+            //string name = (string)stream.ReceiveNext();
+        }
+    }
+
+}
---
+++ Assets/Scripts/playerUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerUI : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+
+    public void UpdateUI(string playerName)
+    {
+        uiText.text = playerName;
+    }
+}
