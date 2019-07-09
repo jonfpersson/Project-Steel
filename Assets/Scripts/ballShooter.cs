978fb93fbff0552b58d28dfea5c01fbbdcad1719
Jonathan <jonfpersson@gmail.com>
2019-07-09 21:42:28 +0200

    Tweaked damage function

--- Assets/Scripts/ballShooter.cs
+++ Assets/Scripts/ballShooter.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ballShooter : MonoBehaviour
 {
     public GameObject projectileAsset;
+
+    public Transform shootpoint;
+    public int damage = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +19,27 @@ public class ballShooter : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject projectile = (GameObject)PhotonNetwork.Instantiate("bullet", transform.position + Camera.main.transform.up * 0.75f + Camera.main.transform.forward * 1.3f, Quaternion.identity, 0);
-            //projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            rb.velocity = Camera.main.transform.forward * 60;
-            
+            /*  GameObject projectile = (GameObject)PhotonNetwork.Instantiate("bullet", transform.position + Camera.main.transform.up * 0.75f + Camera.main.transform.forward * 1.3f, Quaternion.identity, 0);
+              //projectile.transform.position = transform.position + Camera.main.transform.forward * 2;
+              Rigidbody rb = projectile.GetComponent<Rigidbody>();
+              rb.velocity = Camera.main.transform.forward * 60;
+              */
+
+            RaycastHit hit;
+            if(Physics.Raycast(shootpoint.position, shootpoint.forward, out hit))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    PhotonView pView = hit.transform.GetComponent<PhotonView>();
+
+                    if (pView)
+                    {
+                        pView.RPC("ApplyDamage", PhotonTargets.All, damage);
+                        
+                    }
+                }
+            }
+
         }
     }
 }
--- Assets/Scripts/playerNetwork.cs
+++ Assets/Scripts/playerNetwork.cs
@@ -17,15 +17,24 @@ public class playerNetwork : MonoBehaviour
     //private delegate void UpdateUI(string playerName);  
     //private event UpdateUI updateUI;
 
-    string inputName = "Nisse";
+    public int health = 100;
     // Start is called before the first frame update
     void Start()
     {
-        floatingName.text = inputName;
+        floatingName.text = health.ToString();
         pw = GetComponent<PhotonView>();
        // updateUI += FindObjectOfType<playerUI>().UpdateUI;
 
         ini();
+    }
+
+    private void Update()
+    {
+        if (!pw.isMine)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+            health -= 5;
     }
 
     public void ini()
@@ -51,15 +60,21 @@ public class playerNetwork : MonoBehaviour
         //updateUI(inputName);
     }
 
+    [PunRPC]
+    void ApplyDamage(int damage)
+    {
+        health -= damage;
+    }
+
     private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting){
-         //   stream.SendNext(floatingName.text);
+            stream.SendNext(health);
         }
         else if (stream.isReading)
         {
-          //  string name = (string)stream.ReceiveNext();
-         //   floatingName.text = name;
+            int name = (int)stream.ReceiveNext();
+            floatingName.text = name.ToString();
         }
     }
 
